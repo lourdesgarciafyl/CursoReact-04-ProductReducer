b1b51d5f9ce9ac6c0f49a56f8bfac2e5d8014d86
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-08-17 18:56:43 -0300

    primer commit para limpiar la estructura del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navegacion from './components/Navegacion'
+function App() {
+ 
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
