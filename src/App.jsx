c878831f3e5eacb44165a76bb746f01faf3e9266
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-08-17 19:03:30 -0300

    maquetado navbar y footer, y estilos del main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navegacion from './components/Navegacion'
+import FooterPag from './components/FooterPag';
 function App() {
  
   return (
     <>
+    <Navegacion />
+    <section className='mainSection'>
+
+    </section>
+    <FooterPag />
     </>
   )
 }
---
+++ src/components/FooterPag.jsx
@@ -0,0 +1,8 @@
+const FooterPag = () => {
+  return (
+    <footer className="bg-black py-4 text-white text-center">
+      Todos los derechos reservados
+    </footer>
+  );
+};
+export default FooterPag;
