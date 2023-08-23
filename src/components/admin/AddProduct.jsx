c35ac607f20b75e4856ef8255857fe8d0b04e9f5
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-08-23 17:08:44 -0300

    trabajamos con los inputs para que se guarden sus valores y se setee el formulario

--- src/components/admin/AddProduct.jsx
+++ src/components/admin/AddProduct.jsx
@@ -1,46 +1,66 @@
+import { useState } from "react";
+import { useRef } from "react";
+
 const AddProduct = () => {
+  const titleRef = useRef(null);
+  const categoryRef = useRef(null);
+  const priceRef = useRef(null);
+  const descriptionRef = useRef(null);
+  const [form, setForm] = useState({})
 
-
-
-    return (
-  
-      <div className="col-lg-4" >
-          <form>
-              <div className="mb-3">
-                  <input 
-                      type="text" 
-                      className="form-control"  
-                      placeholder="Nombre del producto"
-                      name='title' 
-                  />
-              </div>
-              <div className="mb-3">
-                  <input 
-                      type="text" 
-                      className="form-control"  
-                      placeholder="Categoria"
-                      name='category' 
-                  />
-              </div>
-              <div className="mb-3">
-                  <input 
-                      type="text" 
-                      className="form-control" 
-                      placeholder="Precio"
-                      name='price' 
-                  />
-              </div>
-              <div className="mb-3">
-                  <textarea 
-                      className="form-control" 
-                      rows="3" 
-                      placeholder='Descripcion del producto'
-                      name='description'>
-                  </textarea>
-              </div>
-              <button className='btn btn-info'>Guardar</button>
-          </form>
-      </div>
-    )
+  const onChangeForm = (value, field) => {
+    setForm({
+        ...form,
+       [field] : value
+    })
+    console.log(form)
   }
-  export default AddProduct
+  return (
+    <div className="col-lg-4">
+      <form>
+        <div className="mb-3">
+          <input
+            ref={titleRef}
+            type="text"
+            className="form-control"
+            placeholder="Nombre del producto"
+            name="title"
+            onChange={(event) => onChangeForm(event.target.value, titleRef.current.name)}
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            ref={categoryRef}
+            type="text"
+            className="form-control"
+            placeholder="Categoria"
+            name="category"
+            onChange={(event) => onChangeForm(event.target.value, categoryRef.current.name)}
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            ref={priceRef}
+            type="text"
+            className="form-control"
+            placeholder="Precio"
+            name="price"
+            onChange={(event) => onChangeForm(event.target.value, priceRef.current.name)}
+          />
+        </div>
+        <div className="mb-3">
+          <textarea
+            ref={descriptionRef}
+            className="form-control"
+            rows="3"
+            placeholder="Descripcion del producto"
+            name="description"
+            onChange={(event) => onChangeForm(event.target.value, descriptionRef.current.name)}
+          ></textarea>
+        </div>
+        <button className="btn btn-info">Guardar</button>
+      </form>
+    </div>
+  );
+};
+export default AddProduct;
